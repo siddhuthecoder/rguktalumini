9c363760c54ffd2430ba3a2f048ec435ad0be3e1
Karthik1356 <Sannapukarthik@gmail.com>
2024-02-27 11:54:22 +0530

    firstcommit

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+const navToggle=document.querySelector('.navv-toggle');
+const links=document.querySelector('.links');
+navToggle.addEventListener('click',toggle);
+const toggle=()=>{
+    console.log('Toggle button clicked');
+}
