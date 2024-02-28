7e8337624186e7eb5b37e4147053b8fb67a531cf
siddhu_from_sklm <codewithsiddhu@gmail.com>
2024-02-28 20:07:17 +0530

    fifd

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,20 @@
-const navToggle=document.querySelector('.navv-toggle');
-const links=document.querySelector('.links');
-navToggle.addEventListener('click',toggle);
-const toggle=()=>{
-    console.log('Toggle button clicked');
-}
+const navBar=document.querySelector('#l-items'); // initial nav-bar
+// navBar.classList.toggle('d-none')
+const inbtn=document.querySelector('#buttn') // initial nav-bar toggler button.
+const lc=document.querySelector('.line-container');
+inbtn.addEventListener('click',()=>{
+    vntop.classList.remove('d-none');
+    links.classList.remove('n-links');
+    navBar.classList.toggle('d-none');
+    lc.classList.toggle('d-none');
+})
+const links=document.querySelector('#n-menu'); // pop up nav bar
+const vntop=document.querySelector('#n-topp');  // it is virtual window top bar 
+vntop.classList.add('d-none');
+const vbtn=document.querySelector('.vbtnn')  //Related to button of virtual tab.
+vbtn.addEventListener('click',()=>{
+    vntop.classList.toggle('d-none');
+    links.classList.toggle('n-links');
+    navBar.classList.toggle('d-none');
+    lc.classList.toggle('d-none');
+})
